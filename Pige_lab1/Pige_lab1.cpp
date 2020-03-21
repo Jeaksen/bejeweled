6e00de15e13a71df71a36ad81c162b212c332bf9
Pawel Pohl <pawlomax16@gmail.com>
2020-03-21 19:36:55 +0100

    Window resizing + basic hover.

--- Pige_lab1/Pige_lab1.cpp
+++ Pige_lab1/Pige_lab1.cpp
@@ -13,18 +13,23 @@ HINSTANCE hInst;                                // current instance
 int nCmdShow;
 WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
 WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
-const POINT Sizes[] = { {740, 720 + 60}, {820, 800 + 60}, {860, 840 + 60} };
 const int gridCount[] = { 8, 10, 12 };
-const int gridSizes[] = { 80, 70, 60 };
-int gameSize = 0;
+//TODO change sizes
+const int gridSizes[] = { 70, 60, 50 };
+const int MARGIN = 5, HOVER_INCREASE = 4;
+int gameSize = 2;
 
 // Forward declarations of functions included in this code module:
 ATOM                MyRegisterClass(HINSTANCE hInstance);
 ATOM                MyRegisterChildClass(HINSTANCE hInstance);
 BOOL                InitInstance(HINSTANCE, int);
 LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
-LRESULT CALLBACK ChildWndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+LRESULT CALLBACK    ChildWndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
 INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+void                ResizeWindow(HWND mainHandle);
+void                ChangeGameSize(HWND mainHandle, int size);
+BOOL CALLBACK       EnumKillChild(_In_ HWND   hwnd, _In_ LPARAM lParam);
+
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
                      _In_opt_ HINSTANCE hPrevInstance,
@@ -34,8 +39,6 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     UNREFERENCED_PARAMETER(hPrevInstance);
     UNREFERENCED_PARAMETER(lpCmdLine);
 
-    // TODO: Place code here.
-
     // Initialize global strings
     LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
     LoadStringW(hInstance, IDC_PIGELAB1, szWindowClass, MAX_LOADSTRING);
@@ -43,13 +46,9 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     MyRegisterChildClass(hInstance);
 
     // Perform application initialization:
-    if (!InitInstance (hInstance, nCmdShow))
-    {
-        return FALSE;
-    }
+    if (!InitInstance (hInstance, nCmdShow)) return FALSE;
 
     HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_PIGELAB1));
-
     MSG msg;
 
     // Main message loop:
@@ -112,35 +111,18 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
     hInst = hInstance; // Store instance handle in our global variable
     ::nCmdShow = nCmdShow;
-    int maxWidth = GetSystemMetrics(SM_CXSCREEN);
-    int maxHeight = GetSystemMetrics(SM_CYSCREEN);
-    int dx = (maxWidth - Sizes[gameSize].x) / 2;
-    int dy = (maxHeight - Sizes[gameSize].y) / 2;
 
-    HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW - WS_MAXIMIZEBOX - WS_THICKFRAME, dx, dy, Sizes[gameSize].x, Sizes[gameSize].y, nullptr, nullptr, hInstance, nullptr);
+    HWND hWnd = CreateWindowExW(WS_EX_NOPARENTNOTIFY, szWindowClass, szTitle, WS_OVERLAPPEDWINDOW - WS_MAXIMIZEBOX - WS_THICKFRAME, 0, 0, 0, 0, nullptr, nullptr, hInstance, nullptr);
+    if (!hWnd) return FALSE;
+    ResizeWindow(hWnd);
+    ShowWindow(hWnd, nCmdShow);
+    UpdateWindow(hWnd);
+    return TRUE;
+}
 
-   if (!hWnd) return FALSE;
-
-
-   ShowWindow(hWnd, nCmdShow);
-   UpdateWindow(hWnd);
-   HWND child;
-
-   int x = 5, y = 5;
-   for (size_t i = 0; i < gridCount[gameSize]; i++)
-   {
-       for (size_t j = 0; j < gridCount[gameSize]; j++)
-       {
-           child = CreateWindowW(L"Grid", L"", WS_CHILD | WS_VISIBLE, x, y, gridSizes[gameSize], gridSizes[gameSize], hWnd, nullptr, hInstance, nullptr);
-           ShowWindow(child, nCmdShow);
-           UpdateWindow(child);
-           x += gridSizes[gameSize] + 10;
-       }
-       x = 5;
-       y += gridSizes[gameSize] + 10;
-   }
-
-   return TRUE;
+BOOL CALLBACK EnumKillChild(_In_ HWND   hwnd, _In_ LPARAM lParam)
+{
+    return DestroyWindow(hwnd);
 }
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
@@ -165,32 +147,13 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
                 CloseWindow(hWnd);
                 break;
             case ID_BOARDSIZE_SMALL:
-                gameSize = 0;
-                menu = GetMenu(hWnd);
-                
-                CheckMenuItem(menu, ID_BOARDSIZE_SMALL, MF_CHECKED);
-                CheckMenuItem(menu, ID_BOARDSIZE_MEDIUM, MF_UNCHECKED);
-                CheckMenuItem(menu, ID_BOARDSIZE_BIG, MF_UNCHECKED);
-
+                ChangeGameSize(hWnd, 0);
                 break;
             case ID_BOARDSIZE_MEDIUM:
-                gameSize = 1;
-                menu = GetMenu(hWnd);
-                CheckMenuItem(menu, ID_BOARDSIZE_SMALL, MF_UNCHECKED);
-
-                CheckMenuItem(menu, ID_BOARDSIZE_MEDIUM, MF_CHECKED);
-                CheckMenuItem(menu, ID_BOARDSIZE_BIG, MF_UNCHECKED);
-
+                ChangeGameSize(hWnd, 1);
                 break;
             case ID_BOARDSIZE_BIG:
-                gameSize = 2;
-                menu = GetMenu(hWnd);
-                CheckMenuItem(menu, ID_BOARDSIZE_SMALL, MF_UNCHECKED);
-                CheckMenuItem(menu, ID_BOARDSIZE_MEDIUM, MF_UNCHECKED);
-
-                CheckMenuItem(menu, ID_BOARDSIZE_BIG, MF_CHECKED);
-
-
+                ChangeGameSize(hWnd, 2);
                 break;
             default:
                 return DefWindowProc(hWnd, message, wParam, lParam);
@@ -201,7 +164,6 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         {
             PAINTSTRUCT ps;
             HDC hdc = BeginPaint(hWnd, &ps);
-            // TODO: Add any drawing code that uses hdc here...
             EndPaint(hWnd, &ps);
         }
         break;
@@ -214,42 +176,111 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
     return 0;
 }
 
-void ResizeWindow()
+void ResizeWindow(HWND mainHandle)
 {
+    int maxWidth = GetSystemMetrics(SM_CXSCREEN);
+    int maxHeight = GetSystemMetrics(SM_CYSCREEN);
+    int size = gridCount[gameSize] * (gridSizes[gameSize] + 2 * MARGIN);
+    RECT windowRec{ 0, 0, size, size };
 
+    AdjustWindowRect(&windowRec, WS_OVERLAPPEDWINDOW, true);
+    POINT WindowSize{ windowRec.right - windowRec.left, windowRec.bottom - windowRec.top };
+
+    int dx = (maxWidth - WindowSize.x) / 2;
+    int dy = (maxHeight - WindowSize.y) / 2;
+
+    MoveWindow(mainHandle, dx, dy, WindowSize.x, WindowSize.y, TRUE);
+    EnumChildWindows(mainHandle, EnumKillChild, NULL);
+    UpdateWindow(mainHandle);
+    HWND child;
+
+    int x = 5, y = 5;
+    for (size_t i = 0; i < gridCount[gameSize]; i++)
+    {
+        for (size_t j = 0; j < gridCount[gameSize]; j++)
+        {
+            child = CreateWindowW(L"Grid", L"", WS_CHILD | WS_VISIBLE, x, y, gridSizes[gameSize], gridSizes[gameSize], mainHandle, nullptr, hInst, nullptr);
+            ShowWindow(child, nCmdShow);
+            UpdateWindow(child);
+            x += gridSizes[gameSize] + 10;
+        }
+        x = 5;
+        y += gridSizes[gameSize] + 10;
+    }
+}
+
+void ChangeGameSize(HWND mainHandle, int size)
+{
+    if (gameSize == size) return;
+    HMENU menu = GetMenu(mainHandle);
+
+    switch (gameSize)
+    {
+        case 0: CheckMenuItem(menu, ID_BOARDSIZE_SMALL, MF_UNCHECKED); break;
+        case 1: CheckMenuItem(menu, ID_BOARDSIZE_MEDIUM, MF_UNCHECKED); break;
+        case 2: CheckMenuItem(menu, ID_BOARDSIZE_BIG, MF_UNCHECKED); break;
+    }
+    switch (size)
+    {
+        case 0: CheckMenuItem(menu, ID_BOARDSIZE_SMALL, MF_CHECKED); break;
+        case 1: CheckMenuItem(menu, ID_BOARDSIZE_MEDIUM, MF_CHECKED); break;
+        case 2: CheckMenuItem(menu, ID_BOARDSIZE_BIG, MF_CHECKED); break;
+    }
+    gameSize = size;
+    ResizeWindow(mainHandle);
 }
 
 LRESULT CALLBACK ChildWndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
+    static bool hover = false;
     switch (message)
     {
-    case WM_COMMAND:
-    {
-        int wmId = LOWORD(wParam);
-        // Parse the menu selections:
-        switch (wmId)
+    case WM_MOUSEMOVE:
+        if (!hover)
         {
-        case IDM_ABOUT:
-            DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
-            break;
-        case IDM_EXIT:
-            DestroyWindow(hWnd);
-            break;
-        default:
-            return DefWindowProc(hWnd, message, wParam, lParam);
+            hover = true;
+            TRACKMOUSEEVENT trmev;
+            trmev.cbSize = sizeof(TRACKMOUSEEVENT);
+            trmev.dwFlags = TME_LEAVE;
+            trmev.hwndTrack = hWnd;
+            TrackMouseEvent(&trmev);
+            RECT rect;
+            GetClientRect(hWnd, &rect);
+            POINT spawn{ rect.left, rect.top };
+            MapWindowPoints(hWnd, GetParent(hWnd), &spawn, 1);
+            InflateRect(&rect, HOVER_INCREASE, HOVER_INCREASE);
+            spawn.x -= HOVER_INCREASE;
+            spawn.y -= HOVER_INCREASE;
+            MoveWindow(hWnd, spawn.x, spawn.y, rect.right - rect.left, rect.bottom - rect.top, TRUE);
         }
+        break;
+    case WM_MOUSELEAVE:
+    {
+        hover = false;
+        TRACKMOUSEEVENT trmev;
+        trmev.cbSize = sizeof(TRACKMOUSEEVENT);
+        trmev.dwFlags = TME_CANCEL | TME_LEAVE;
+        trmev.hwndTrack = hWnd;
+        TrackMouseEvent(&trmev);
+        RECT rect;
+        GetClientRect(hWnd, &rect);
+        POINT spawn{ rect.left, rect.top };
+        MapWindowPoints(hWnd, GetParent(hWnd), &spawn, 1);
+        InflateRect(&rect, -HOVER_INCREASE, -HOVER_INCREASE);
+        spawn.x += HOVER_INCREASE;
+        spawn.y += HOVER_INCREASE;
+        MoveWindow(hWnd, spawn.x, spawn.y, rect.right - rect.left, rect.bottom - rect.top, TRUE);
     }
-    break;
+        break;
+
     case WM_PAINT:
     {
         PAINTSTRUCT ps;
         HDC hdc = BeginPaint(hWnd, &ps);
-        // TODO: Add any drawing code that uses hdc here...
         EndPaint(hWnd, &ps);
     }
     break;
     case WM_DESTROY:
-        PostQuitMessage(0);
         break;
     default:
         return DefWindowProc(hWnd, message, wParam, lParam);
@@ -258,7 +289,7 @@ LRESULT CALLBACK ChildWndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lPa
 }
 
 
-// Message handler for about box.
+
 INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 {
     UNREFERENCED_PARAMETER(lParam);
